3bb867724b1ed4129510938a37cb11536809895d
Sangee-R <109620687+Sangee-R@users.noreply.github.com>
2022-07-23 19:29:27 +0530

    Add files via upload

---
+++ Task4.js
@@ -0,0 +1,12 @@
+var obj1 = {
+    name:"person",
+    age:5
+};
+
+var obj2 = {
+    age:5,
+    name:"person",
+    
+};
+console.log("json is Equals:" + isEqualJson(obj1,obj2));
+alert("json is equals: "+ isEqualJson(obj1,obj2));
