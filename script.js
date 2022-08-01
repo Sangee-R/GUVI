b8050a7ce2c85d02cfb30442578d7df3861a037c
Sangee-R <109620687+Sangee-R@users.noreply.github.com>
2022-08-01 21:06:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+//Get all the countries from Asia continent /region using Filter function
+
+var request=new XMLHttpRequest();
+request.open('GET','https://restcountries.com/v3.1/all','true');
+request.send();
+request.onload=function (){
+    var countryData=JSON.parse(this.response);
+    const asia=countryData.filter((element)=>{
+        if(element.region==='Asia'){
+            return element.name;
+        }
+    })
+    console.log(asia);}
+
+
+    //Print the following details name, capital, flag using forEach function.
+
+var request=new XMLHttpRequest();
+request.open('GET','https://restcountries.com/v3.1/all','true');
+request.send();
+request.onload=function (){
+    var countryData=JSON.parse(this.response);
+    countryData.forEach((element)=>{
+        console.log(element.flags);
+    })}
